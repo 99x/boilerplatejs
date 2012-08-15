bd7b37d3a91419a108d231f02521cd986071903e
slayerjay <99xcjwe@.99x.local>
2012-08-15 18:10:59 +0530

    Added a browser version check function

--- libs/boilerplate/groundwork.js
+++ libs/boilerplate/groundwork.js
@@ -2,4 +2,8 @@
 	if(window.location.protocol === 'file:') {
 		window.alert("Samples in BoilerplateJS might not work when opened directly from the file system. Please deploy in to a web server and access with 'http://' url", "Samples may not work");
 	}
+
+	if($.browser.msie && parseInt($.browser.version, 10) < 9) {
+		window.alert("You browser version is not supported. Please update to Internet Explorer 9 or onwards, or use an HTML5 compatible browser such as Chrome or Firefox.", "Samples may not work");
+	}
 })();
