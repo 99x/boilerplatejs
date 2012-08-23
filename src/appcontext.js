054ace68e56da1038e4dfc8878c6481e26aa1330
hasith <hasithy@99x.lk>
2012-08-23 10:59:37 +0530

    code cleanup

--- src/appcontext.js
+++ src/appcontext.js
@@ -1,30 +1,30 @@
 ﻿/*
- * This file holds the function (or you may call it a 'class' if you are from .NET or Java world)
- * to create a root application context. 'Boiler.Context' can be nested to create a hierachy of contexts 
- * for complex implementations. Below we use requirejs to import following files 
- *      'Boiler'      : Namespace alias for accessing the core boilerplate functions
- *      './settings'    : The global setting ( to make available for all children contexts)
- *      './modules'     : The object containing all sub module calsses
- *
- * Note: when we define the variables in AMD callback function, we use PascalCase for namespaces
- * and classes ('Boiler' in the case), whereas object instances ('settings' and 'modules') 
- * are represented with camelCase variable names.
- */
-define(["Boiler", "./settings", "./modules/modules"], function (Boiler, settings, moduleContexts) {
+* This file holds the function (or you may call it a 'class' if you are from .NET or Java world)
+* to create a root application context. 'Boiler.Context' can be nested to create a hierachy of contexts
+* for complex implementations. Below we use requirejs to import following files
+*      'Boiler'      : Namespace alias for accessing the core boilerplate functions
+*      './settings'    : The global setting ( to make available for all children contexts)
+*      './modules'     : The object containing all sub module calsses
+*
+* Note: when we define the variables in AMD callback function, we use PascalCase for namespaces
+* and classes ('Boiler' in the case), whereas object instances ('settings' and 'modules')
+* are represented with camelCase variable names.
+*/
 
-    /*
-     * Following is our function representing GlobalContext 'class'. Others can import this 
-     * script as a AMD module and they will obtain the function to be used as a 'class' in 
-     * creating instances.
-     */
-    var AppContext = function () {
-        //Lets use core Boiler classes to create our global context instance
-        var appContext = new Boiler.Context();
-        //now lets add settings, which are to be available as global settings
-        appContext.addSettings(settings);
-        //here we load the sub modules of the global context
-        appContext.loadChildContexts(moduleContexts);
-    };
-    
-    return AppContext;
+define(["Boiler", "./settings", "./modules/modules"], function(Boiler, settings, moduleContexts) {
+
+	/**
+	 * AppContext class to create a global context that
+	 * will act as the parent for module contexts.
+	 */
+	var AppContext = function() {
+		//Create our root context instance
+		var appContext = new Boiler.Context();
+		//now lets add global settings
+		appContext.addSettings(settings);
+		//here we load the sub modules of the global context
+		appContext.loadChildContexts(moduleContexts);
+	};
+
+	return AppContext;
 });
