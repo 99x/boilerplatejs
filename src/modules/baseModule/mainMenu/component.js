8f2adcde6f864581086b016070be85ce90774c48
slayerjay <99xcjwe@.99x.local>
2012-08-24 18:23:41 +0530

    Added images to menus

--- src/modules/baseModule/mainMenu/component.js
+++ src/modules/baseModule/mainMenu/component.js
@@ -1,10 +1,10 @@
-define(['Boiler', 'text!./view.html', 'i18n!./nls/resources'], function(Boiler, template, nls) {
+define(['Boiler', 'text!./view.html', 'i18n!./nls/resources', 'text!./style.css'], function(Boiler, template, nls, style) {
 
 	var RouteHandler = function(moduleContext) {
 		var panel = null;
 		return {
 			activate : function(parent) {
-				panel = new Boiler.ViewTemplate(parent, template, nls);
+				panel = new Boiler.ViewTemplate(parent, template, nls, style);
 			},
 
 			deactivate : function() {
