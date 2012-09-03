f5a76a1288fb4f4a6a75e6a62480bd8f3c34129b
hasith <hasithy@99x.lk>
2012-09-03 22:47:02 +0530

    added 'path' require dependency to the optimizer

--- tools/optimizer/boilerplatejs.build.js
+++ tools/optimizer/boilerplatejs.build.js
@@ -9,6 +9,7 @@
 		domReady : '../libs/require/domReady',
 		Boiler : './core/_boiler_',
 		text : "../libs/require/text",
-		i18n : "../libs/require/i18n"
+		i18n : "../libs/require/i18n",
+		path : "../libs/require/path"
 	},
 })
