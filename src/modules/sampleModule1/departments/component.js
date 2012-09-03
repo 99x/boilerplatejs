4a69b3c83d15aacf129bc6d3d291c508454ff59a
hasith <hasithy@99x.lk>
2012-09-03 12:02:12 +0530

    added more code comments on UI components. Added initialize method to make UI components stateless.

--- src/modules/sampleModule1/departments/component.js
+++ src/modules/sampleModule1/departments/component.js
@@ -1,24 +1,30 @@
 define(['require', 'Boiler', 'text!./view.html', './viewmodel', 'i18n!./nls/resources'], function(require, Boiler, template, ViewModel, nls) {
 
 	var Component = function(moduleContext) {
-		
 
 		var panel, vm = null;
-		
+
 		this.activate = function(parent, params) {
 			if (!panel) {
+				//attach the template to the DOM
 				panel = new Boiler.ViewTemplate(parent, template, nls);
+				//attach the stylesheet of this component
 				Boiler.ViewTemplate.setStyleLink("DEPAERTMENT_PANEL_CSS", require.toUrl('./style.css'));
+				//create out viewmodel
 				vm = new ViewModel(moduleContext);
+				//use knockout for bindings
 				ko.applyBindings(vm, panel.getDomElement());
 			}
-			
-			vm.setNewDepartmentName(params.name);
+			/* It is recommended to keep your views stateless as much as possible.
+			 * Everytime user access the view, we should reload it from server to avoin stale data.
+			 */
+			vm.initialize(params.name);
+			//make the component visible incase it is hidden
 			panel.show();
 		}
 
 		this.deactivate = function() {
-			if(panel) {
+			if (panel) {
 				panel.hide();
 			}
 		}
--- src/modules/sampleModule1/departments/viewmodel.js
+++ src/modules/sampleModule1/departments/viewmodel.js
@@ -1,43 +1,43 @@
-define([], function () {
-	
+define([], function() {
+
 	var DEFAULT_DEPT_NAME = 'Boiler Department';
 
-    var ViewModel = function (moduleContext) {
+	var ViewModel = function(moduleContext) {
 
-        var self = this;
-        this.itemToAdd = ko.observable(DEFAULT_DEPT_NAME);
-        this.allItems = ko.observableArray([]);
-        this.selectedItems = ko.observableArray();
-        
-        this.setNewDepartmentName = function(name){
-        	if (name) {
-        		this.itemToAdd(name);
-        	} else {
-        		this.itemToAdd(DEFAULT_DEPT_NAME);
-        	}
-        };
+		var self = this;
+		this.itemToAdd = ko.observable(DEFAULT_DEPT_NAME);
+		this.allItems = ko.observableArray([]);
+		this.selectedItems = ko.observableArray();
 
+		this.initialize = function(deptName) {
+			if (deptName) {
+				this.itemToAdd(deptName);
+			} else {
+				this.itemToAdd(DEFAULT_DEPT_NAME);
+			}
+			$.getJSON(moduleContext.getSettings().urls.departments, function(result) {
+				self.allItems(result);
+			});
+		};
 
-        this.addItem = function () {
-            if ((this.itemToAdd() != "") && (this.allItems.indexOf(this.itemToAdd()) < 0)) // Prevent blanks and duplicates
-                this.allItems.push(this.itemToAdd());
-            this.itemToAdd(""); // Clear the text box
-        };
+		this.addItem = function() {
+			if ((this.itemToAdd() != "") && (this.allItems.indexOf(this.itemToAdd()) < 0))// Prevent blanks and duplicates
+				this.allItems.push(this.itemToAdd());
+			this.itemToAdd("");
+			// Clear the text box
+		};
 
-        this.removeSelected = function () {
-            this.allItems.removeAll(this.selectedItems());
-            this.selectedItems([]); // Clear selection
-        };
+		this.removeSelected = function() {
+			this.allItems.removeAll(this.selectedItems());
+			this.selectedItems([]);
+			// Clear selection
+		};
 
-        this.sortItems = function () {
-            this.allItems.sort();
-        };
+		this.sortItems = function() {
+			this.allItems.sort();
+		};
 
-        $.getJSON(moduleContext.getSettings().urls.departments, function (result) {
-            self.allItems(result);
-        });
+	};
 
-    };
-    
-    return ViewModel;
+	return ViewModel;
 });
