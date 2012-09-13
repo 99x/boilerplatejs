4e9fe04ff00a37f2f0507fd8686e056735a429fe
hasith <hasithy@99x.lk>
2012-09-13 14:35:10 +0530

    clickcounter example usability improvements

--- src/modules/sampleModule1/clickCounter/lottery/viewmodel.js
+++ src/modules/sampleModule1/clickCounter/lottery/viewmodel.js
@@ -3,10 +3,13 @@ define([], function () {
     var ViewModel = function (moduleContext) {
     	var self = this;
     	this.hasWon = ko.observable(false);
+    	this.luckyNumber = ko.observable();
+    	this.yourNumber = ko.observable();
     	
 	    moduleContext.listen("LOTTERY_ACTIVITY", function(activityNumber) {
-	    	var randomNum = Math.floor(Math.random() * 3) + 1;
-	    	self.hasWon(randomNum === activityNumber);
+	    	self.luckyNumber(Math.floor(Math.random() * 3) + 1);
+	    	self.yourNumber(activityNumber);
+	    	self.hasWon(self.luckyNumber() === self.yourNumber());
 	    });
     };
     
