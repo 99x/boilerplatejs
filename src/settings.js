711db703f809cabc6f7be4289eac4a707e9cf15a
VeXHarbinger <alexpearlstein@gmail.com>
2012-09-19 06:49:43 -0300

    Update src/settings.js
    Corrected spelling mistake.

--- src/settings.js
+++ src/settings.js
@@ -2,7 +2,7 @@
 * This AMD module define a object containing settings as its properties. This settings file is 
 * getting loaded to GlobalContext, so that settings defined here are available to all 
 * sub contexts.
-* Settings can be just simple literal vatiables or objects having complex definitions.
+* Settings can be just simple literal variables or objects having complex definitions.
 */
 define({
     appName: "BoilerplateJS",
