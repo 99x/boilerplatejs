b958a026f754d4f1012bbb9a8ff710cf479a9296
VeXHarbinger <alexpearlstein@gmail.com>
2012-09-19 07:45:13 -0300

    Update src/modules/modules.js

--- src/modules/modules.js
+++ src/modules/modules.js
@@ -4,7 +4,7 @@ define(function(require) {
 	 * Return an array containing all modules classes that needs to be initiated.
 	 * We use the 'require' function of requirejs to get the relevant module context classes.
 	 * This could be done of course, by passing those scripts as dependencies to 
-	 * the 'define' function above. But following model is bit simpler to read.
+	 * the 'define' function above. But following model is a bit simpler to read.
 	 */
 	return [
             require('./baseModule/module'),
